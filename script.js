24b6f6d7ff069ef6e8c0f36ae0c714c52a26bf2b
Ryan <ryan.strider@escola.pr.gov.br>
2025-08-14 11:57:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const Cardapio = [
-    { Id: "carne", Pastel: "Pastel de Carne", Preço: 15.00                  },
-    { Id: "frango", Pastel: "Pastel de Frango", Preço: 15.00                },
-    { Id: "chocolate", Pastel: "Pastel de Chocolate", Preço: 20.00          },
-    { Id: "queijo", Pastel: "Pastel de Queijo", Preço: 15.00                },
-    { Id: "carneQueijo", Pastel: "Pastel de Carne com Queijo", Preço: 15.00 },
-    { Id: "cheddar", Pastel: "Pastel de Carne com Cheddar", Preço: 20.00    }
-]
+    { Id: "carne", Pastel: " Pastel de Carne ", Preço: 15.00                  },
+    { Id: "frango", Pastel: " Pastel de Frango ", Preço: 15.00                },
+    { Id: "chocolate", Pastel: " Pastel de Chocolate ", Preço: 20.00          },
+    { Id: "queijo", Pastel: " Pastel de Queijo ", Preço: 15.00                },
+    { Id: "carneQueijo", Pastel: " Pastel de Carne com Queijo ", Preço: 15.00 },
+    { Id: "cheddar", Pastel: " Pastel de Carne com Cheddar ", Preço: 20.00    }
+];
 
 const Tamanho = [
-    { Id: "pequeno", Tamanho: "Pequeno", Preço: 5.00 },
-    { Id: "médio", Tamanho: "Médio", Preço: 7.00     },
-    { Id: "grande", Tamanho: "Grande", Preço: 10.00  }
-]
+    { Id: "pequeno", Tamanho: " Pequeno ", Preço: 5.00 },
+    { Id: "médio", Tamanho: " Médio ", Preço: 7.00     },
+    { Id: "grande", Tamanho: " Grande ", Preço: 10.00  }
+];
 
 const Receber = [
-    { Id: "entrega", Método: "Entrega", Taxa: 5.00  },
-    { Id: "retirada", Método: "Retirada", Taxa: 0.00 }
-]
+    { Id: "entrega", Método: " Entrega ", Taxa: 5.00  },
+    { Id: "retirada", Método: " Retirada ", Taxa: 0.00 }
+];
 
 const Pagamento = [
-    { Id: "crédito", Método: "Crédito"   },
-    { Id: "débito", Método: "Débito"     },
-    { Id: "pix", Método: "Pix"           },
-    { Id: "dinheiro", Método: "dinheiro" }
-] 
+    { Id: "crédito", Método: " Crédito "   },
+    { Id: "débito", Método: " Débito "     },
+    { Id: "pix", Método: " Pix "           },
+    { Id: "dinheiro", Método: " Dinheiro " }
+];
 
 const Informações = [
-    { Id: "endereço", Endereço: " R. Lisboa, 385 - Jardim Alice I, Foz do Iguaçu "},
-    { Id: "telefone", Telefone: "(45)9914501514"                                  }
-]
+    { Id: "endereço", Endereço: " R. Lisboa, 385 - Jardim Alice I, Foz do Iguaçu " },
+    { Id: "telefone", Telefone: " (45)9914501514 "                                 }
+];
