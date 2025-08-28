0310a3045f2d8305e530e272860958c6915950f4
Ryan <ryan.strider@escola.pr.gov.br>
2025-08-28 08:43:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ const tamanho_loja = [
     { id: "grande", tamanho: " Grande ", preço: 10.00  }
 ];
 
+const entrega_loja = [
+    { id: "entrega", tipo: "Entrega", preço: 5.00  },
+    { id: "buscar", tipo: "Busca", preço: 0.00   }
+]
+
 function preecher(){
     const selectCardapio = document.getElementById("cardapio");
      for (let i = 0; i < cardapio_loja.length; i++) {
@@ -31,5 +36,34 @@ function preecher(){
          option.textContent = item.tamanho.trim();
          selectTamanho.appendChild(option);
 }
+    const selectEntrega = document.getElementById("entrega");
+     for (let i = 0; i < entrega_loja.length; i ++) {
+         const item = entrega_loja[i];
+         const option = document.createElement("option");
+         option.value = item.id;
+         option.textContent = item.tipo.trim();
+         selectEntrega.appendChild(option);
+     }
 }
 preecher();
+
+function procurarPorId(lista, idProcurado) {
+    for (let i = 0; i < lista.lenght; i++) {
+        if (lista[i].id === idProcurado) {
+            return lista[i];
+        }
+    }
+    return null;
+}
+preencherOpcoes();
+
+function gerarRelatorio() {
+      const nome = document.getElementById("nome").value;   
+      const cpf = document.getElementById("cpf").value;
+      const endereco = document.getElementById("endereco").value;
+      const cardapio = parseInt(document.getElementById("cardapio").value);
+      const tamanho = parseInt(document.getElementById("tamanho").value);
+      const entrega = parseInt(document.getElementById("entrega").value);
+
+      var 
+}
